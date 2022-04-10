13c151e3bdb19f84861904ca85e3aeec3c5d8fb3
ildar <ildar.q@mail.ru>
2022-04-10 21:46:32 +0300

    test to git

--- tests/test_images.py
+++ tests/test_images.py
@@ -12,18 +12,15 @@ def test_yandex_images(setup):
     first_image = yandex_page.get_first_image()
     first_image_text = yandex_page.get_image_text()
     first_image.click()
-    link_1 = yandex_page.get_current_url()
     search_text = yandex_page.get_search_text()
     assert first_image_text == search_text, 'image text and search text didnt match'
     first_image_in_category = yandex_page.get_first_image_in_category()
     first_image_in_category.click()
     yandex_page.wait_url_change()
-    link_2 = yandex_page.get_current_url()
-    assert link_1 != link_2, 'opened same page'
     image = yandex_page.get_image()
     image_source1 = image.get_attribute("src")
     yandex_page.slide_right()
     yandex_page.slide_left()
     image = yandex_page.get_image()
     image_source2 = image.get_attribute("src")
-    assert image_source1 == image_source2, 'images didnt match'
+    assert image_source1 == image_source2, 'images src didnt match'
