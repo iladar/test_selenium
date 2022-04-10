c3b5661d819071e578eaa408e8187df6bb8cadea
ildar <ildar.q@mail.ru>
2022-04-10 20:22:14 +0300

    test to git

--- tests/test_search.py
+++ tests/test_search.py
@@ -10,10 +10,12 @@ def test_yandex_search(setup):
     assert (yandex_page.is_present((By.CSS_SELECTOR, '.mini-suggest__popup-content'))), 'didnt find suggest block'
     yandex_page.start_search()
     assert (yandex_page.is_present((By.ID, 'search-result'))), 'no search results'
-    i = 1
     links = yandex_page.find_search_links()
+    link_finded = False
     for link in links:
         url = yandex_page.get_link_url(link)
-        assert url.find('tensor.ru') > 0, f'link to another site in {i} position - {url}'
-        i = i + 1
+        if url.find('tensor.ru') > 0:
+            link_finded=True
+    assert link_finded, 'links not finded'
+
 
