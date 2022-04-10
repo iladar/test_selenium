0b70392dbc2d9b53b3ffe2decabfb3113a8086b3
ildar <ildar.q@mail.ru>
2022-04-10 20:48:16 +0300

    test to git

--- tests/test_images.py
+++ tests/test_images.py
@@ -5,17 +5,21 @@ from selenium.webdriver.common.by import By
 def test_yandex_images(setup):
     yandex_page = YandexImages(setup)
     yandex_page.go_to_site()
-    assert yandex_page.is_present((By.CSS_SELECTOR, '.services-new__icon.services-new__icon_images')), 'no element'
+    assert yandex_page.is_present((By.CSS_SELECTOR, '.services-new__icon.services-new__icon_images')), 'no image link'
     yandex_page.go_to_images_page()
     yandex_page.change_window()
     assert yandex_page.get_current_url().startswith("https://yandex.ru/images/"), 'didnt open yandex.ru/images/'
     first_image = yandex_page.get_first_image()
     first_image_text = yandex_page.get_image_text()
     first_image.click()
+    link_1 = yandex_page.get_current_url()
     search_text = yandex_page.get_search_text()
     assert first_image_text == search_text, 'image text and search text didnt match'
     first_image_in_category = yandex_page.get_first_image_in_category()
     first_image_in_category.click()
+    yandex_page.change_window()
+    link_2 = yandex_page.get_current_url()
+    assert link_1 != link_2, 'opened same page'
     image = yandex_page.get_image()
     image_source1 = image.get_attribute("src")
     yandex_page.slide_right()
